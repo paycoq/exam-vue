0ff253b219be0bc8c2d7925491f7d1d08107b74c
조성현 <mrjozzz@paycoq.com>
2022-06-08 13:05:28 +0900

    오류 화면 만듬

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import ErrorView from "@/views/ErrorView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/error",
+    name: "error",
+    component: ErrorView,
   },
   {
     path: "/about",
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="error">
+    <h1>오류가 발생했습니다.</h1>
+  </div>
+</template>
